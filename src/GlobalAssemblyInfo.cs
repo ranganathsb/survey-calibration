81b2a76aa2c74638a0c8d0f5e4bca1fd0b2c09d5
unknown <saktanorovich@saktanorovich.scnsoft.com>
2015-09-01 16:51:47 +0300

    initial commit

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Home")]
+[assembly: AssemblyProduct("Calibration Module")]
+[assembly: AssemblyCopyright("Copyright © Home 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
